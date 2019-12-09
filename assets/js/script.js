173324c4e8ac8f7d265831f0556f917088a5648c
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2019-12-09 21:11:38 +0400

    write navbar,slider,card,and little bit modal box

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".close").click(function(){
         $("#alert-box").addClass("d-none");
     });
+
+    $(".search").click(function(){
+        $(".search-panel").toggleClass("d-block")
+    });
 });

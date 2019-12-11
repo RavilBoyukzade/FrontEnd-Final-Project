6e42529f4b6cd452a01481c1159ca34ea6865714
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2019-12-11 14:01:20 +0400

    most popular categories ve our courses sesiyakari yigdim

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
-$(document).ready(function(){
-    $(".close").click(function(){
+$(document).ready(function () {
+    $(".close").click(function () {
         $("#alert-box").addClass("d-none");
     });
 
-    $(".search").click(function(){
+    $(".search").click(function () {
         $(".search-panel").toggleClass("d-block")
     });
+
+    $(window).on('scroll' ,function () {
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if(scroll >= 100){
+            $("nav").addClass("nav-fix");
+        }
+        else{
+            $("nav").removeClass("nav-fix");
+        }
+    });
+
+    // $('.owl-carousel').slick({
+    //     infinite: true,
+    //     slidesToShow: 6,
+    //     slidesToScroll: 4
+    // });
 });

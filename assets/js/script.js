697ea6486f6fc7bacc4621e46fc1497fd75b04cc
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2019-12-12 19:24:46 +0400

    add isotope plugin to our gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
+    // close discount section
     $(".close").click(function () {
         $("#alert-box").addClass("d-none");
     });
-
+// serch btn
     $(".search").click(function () {
         $(".search-panel").toggleClass("d-block")
     });
-
+// scroll to nav bar fix
     $(window).on('scroll' ,function () {
         var scroll = $(window).scrollTop();
         console.log(scroll);
@@ -17,7 +18,7 @@ $(document).ready(function () {
             $("nav").removeClass("nav-fix");
         }
     });
-
+// carousel
     $('.owl-carousel').slick({
         arrows:false,
         infinite: true,
@@ -25,9 +26,25 @@ $(document).ready(function () {
         slidesToScroll: 4,
         autoplay:true
     });
-
+// counter
     $(".counter").counterUp({
         delay: 10,
         time: 1000
     });
+// Isotope
+    $(".gallery-item").isotope({
+        itemSelector:".item",
+        layoutMode:"fitRows"
+    });
+    $(".buttons ul li a").click(function(){
+        $(".buttons ul li a").removeClass("active");
+        $(this).addClass("active");
+
+        var selector =$(this).attr("data-filter");
+        $(".gallery-item").isotope({
+            filter: selector
+        });
+        return false;
+    });
+
 });
